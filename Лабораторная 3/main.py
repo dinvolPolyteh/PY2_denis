917d19061a8dc9137a6eaa51be4c7781637acfa8
dinvolPolyteh <115411697+dinvolPolyteh@users.noreply.github.com>
2023-01-24 16:15:28 +0300

    Лабораторная работа 3

--- Лабораторная 3/main.py
+++ Лабораторная 3/main.py
@@ -1,8 +1,16 @@
 class Book:
     """ Базовый класс книги. """
     def __init__(self, name: str, author: str):
-        self.name = name
-        self.author = author
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @property
+    def author(self) -> str:
+        return self._author
 
     def __str__(self):
         return f"Книга {self.name}. Автор {self.author}"
@@ -11,21 +19,42 @@ class Book:
         return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r})"
 
 
-class PaperBook:
-    def __init__(self, name: str, author: str, pages: int):
-        self.name = name
-        self.author = author
+class PaperBook(Book):
+    def __init__(self, name: str, author: str,pages: int):
+        super().__init__(name,author)
         self.pages = pages
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def pages(self) -> int:
+        return self._pages
 
+    @pages.setter
+    def pages(self,new_pages:int) -> None:
+        if not isinstance(new_pages,int) :
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц не может быть меньше или равно 0")
+        self._pages = new_pages
 
-class AudioBook:
-    def __init__(self, name: str, author: str, duration: float):
-        self.name = name
-        self.author = author
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, pages={self.pages!r})"
+
+class AudioBook(Book):
+    def __init__(self,name: str, author: str, duration: float):
+        super().__init__(name, author)
         self.duration = duration
 
-    def __str__(self):
-        return f"Книга {self.name}. Автор {self.author}"
+    @property
+    def duration(self) -> float:
+        return self._duration
+
+    @duration.setter
+    def duration(self,new_duration:float) -> None:
+        if not isinstance(new_duration,float):
+            raise TypeError("Длительность должна быть типа float")
+        if new_duration <= 0:
+            raise ValueError("Длительность не может быть меньше или равна 0")
+        self._duration = new_duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self.name!r}, author={self.author!r}, duration={self.duration!r})"
